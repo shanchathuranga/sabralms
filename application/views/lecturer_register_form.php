36fb8eb5bfa896973c3614cfb8fe68897533f173
shanchathuranga <shanchathuranga@gmail.com>
2015-02-09 17:55:58 +0000

    re factored

---
+++ application/views/lecturer_register_form.php
@@ -0,0 +1,27 @@
+<div class="grid_3">
+	&nbsp;
+</div>
+<div class="grid_9">
+	<h2>Lecturer Registration Form</h2>
+	<form action="<?php echo base_url(); ?>admin/lecturer_regrister" method="post">
+		<label for="first_name">First Name:</label>
+		<br/>
+		<input type="text" name="first_name" id="FName" />
+		<br/>
+		<label for="last_name">Last Name:</label>
+		<br/>
+		<input type="text" name="LName" id="LName" />
+		<br/>
+		<label for="reg_no">Reg No:</label>
+		<br/>
+		<input type="text" name="reg_no" id="RegNo">
+		<br/>
+		<label for="reg_no">Subject:</label>
+		<br/>
+		<input type="text" name="subject" id="subject">
+		<br/>
+		<input type="button" name="button" value="Register">
+	</form>
+</div>
+
+
