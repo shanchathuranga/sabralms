2990a3edadf46a099f7d1bb24fb35c5810e68b27
shanchathuranga@gmail.com <shanchathuranga@gmail.com@e7e17b5c-1a57-c605-3fa1-0dffe02cf9ac>
2015-02-09 17:55:58 +0000

    re factored

--- trunk/application/controllers/admin.php
+++ trunk/application/controllers/admin.php
@@ -2,66 +2,66 @@
 
 class Admin extends CI_Controller {
 
-    public function index() {
-        
-    }
+	public function index() {
+		
+	}
 
-    public function student_form() {
-        $this->load->model('degree_model');
-        $data['main_content'] = "student_register_form";
-        $this->load->view("layouts/main", $data);
-    }
+	public function student_form() {
+		$this->load->model('degree_model');
+		$data['main_content'] = "student_register_form";
+		$this->load->view("layouts/main", $data);
+	}
 
-    public function student_regrister() {
-        // Add data to the database
-        $this->load->model('student_model');
-        $this->student_model->insert_student();
+	public function student_regrister() {
+		// Add data to the database
+		$this->load->model('student_model');
+		$this->student_model->insert_student();
 
-        redirect(base_url() . 'admin/student_form');
-    }
+		redirect(base_url() . 'admin/student_form');
+	}
+	
+	public function lecturer_form() {
+		$data['main_content'] = "lecturer_register_form";
+		$this->load->view("layouts/main", $data);
+	}
 
-    public function Lecturer_Reg() {
-        $data['main_content'] = "lecturer_Reg";
-        $this->load->view("layouts/main", $data);
-    }
+	public function lecturer_register() {
+		// TODO
+	}
 
-    public function Site_news() {
-        $data['main_content'] = "site_news";
-        $this->load->view("layouts/main", $data);
-    }
-    
-    public function course_form() {
-        //$this->load->model('degree_model');
-        $data['main_content'] = "course_register_form";
-        $this->load->view("layouts/main", $data);
-    }
-      public function course_register() 
-    {
-        // Add data to the database
-        $this->load->model('course_model');
-        $this->course_model->insert_course();
+	public function Site_news() {
+		$data['main_content'] = "site_news";
+		$this->load->view("layouts/main", $data);
+	}
 
-        redirect(base_url() . 'admin/course_form');
-    }
-    
-    
-    
-    
-     public function degree_regrister() {
-        // Add data to the database
-        $this->load->model('degree_model');
-        $this->course_model->insert_degree();
+	public function course_form() {
+		//$this->load->model('degree_model');
+		$data['main_content'] = "course_register_form";
+		$this->load->view("layouts/main", $data);
+	}
 
-        redirect(base_url() . 'admin/degree_form');
-    }
-    
-    public function degree_form() {
-        //$this->load->model('degree_model');
-        $data['main_content'] = "course_register_form";
-        $this->load->view("layouts/main", $data);
-    }
-    
-    
+	public function course_register() {
+		// Add data to the database
+		$this->load->model('course_model');
+		$this->course_model->insert_course();
+
+		redirect(base_url() . 'admin/course_form');
+	}
+
+	public function degree_regrister() {
+		// Add data to the database
+		$this->load->model('degree_model');
+		$this->course_model->insert_degree();
+
+		redirect(base_url() . 'admin/degree_form');
+	}
+
+	public function degree_form() {
+		//$this->load->model('degree_model');
+		$data['main_content'] = "course_register_form";
+		$this->load->view("layouts/main", $data);
+	}
+
 }
 
 ?>
