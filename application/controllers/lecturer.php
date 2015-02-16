b824196900c5a4de27433a1f18aa667e0a6547ca
shanchathuranga <shanchathuranga@gmail.com>
2015-02-16 16:52:43 +0000

    topic added

--- application/controllers/lecturer.php
+++ application/controllers/lecturer.php
@@ -22,11 +22,23 @@ class Lecturer extends CI_Controller
 		{
 			redirect(base_url().'lecturer');
 		}
-                $this->load->model("lecturer_model");
+		$this->load->model('course_model');
+		$this->load->model('topic_model');
+		
 		$data['main_content'] = "course_view";
 		$data['course_code'] = base64_decode(urldecode($course_code_encoded));
 		$this->load->view("layouts/main", $data);
                 
+	}
+	
+	public function add_new_topic()
+	{
+		$this->load->model('topic_model');
+		$this->topic_model->insert_topic();
+		
+		$data['main_content'] = "course_view";
+		$data['course_code'] = $this->input->post('course_code');
+		$this->load->view("layouts/main", $data);
 	}
 
 	public function note_upload_view()
--- application/views/course_view.php
+++ application/views/course_view.php
@@ -1,21 +1,56 @@
-    
-    <table width="400px" border="1">
-        <thead>
-            <th>Course code</th>
-            <th>lecturer reg no</th>
-            
-        </thead>
-        <tbody>
-	<?php  
-        //$this->lecturer_model->getAll();
-        $result=$this->lecturer_model->getAll();
-        foreach ($result as $row) { ?>
-	<tr>
-	<td><?= $row->course_code ?></td>
-	<td><?= $row->lecturer_reg_no ?></td>
+<?php
+	//$course_code available
+	$lecturer_reg_no = $this->session->userdata('user_reg_no');
+	$topics = $this->topic_model->get_all_topics_by_course_by_lecturer($course_code, $lecturer_reg_no);
+	$course = $this->course_model->get_course_by_code($course_code);
+?>
+
+<div class="grid_3">
+	&nbsp;
+</div>
+<div class="grid_9">
+	<?php
 	
-	</tr>
-	<?php } ?>
-        </tbody>
-    </table>
-   
+	if ($course)
+	{
+		echo '<h6>'.$course->course_title.'</h6>';
+	}
+	if ($topics)
+	{
+		foreach ($topics as $topic)
+		{
+			echo '<p>';
+			echo '<strong>'.$topic->order.'. '.$topic->topic_title.'</strong>';
+			echo '<br/>';
+			echo $topic->topic_description;
+			echo '<br/>';
+			echo '<a href="#">Update topic</a>';
+			echo '<br/>';
+			echo '<a href="#">Add new attachment</a>';
+			echo '</p>';
+		}
+		echo '<hr/>';
+	}
+	?>
+	
+	<div>
+		<strong>Add new topic to this Course</strong><br/><br/>
+		<form action="<?php echo base_url().'lecturer/add_new_topic'; ?>" method="post">
+			<label for="topic_title">Topic Title</label>
+			<br/>
+			<input type="text" name="topic_title" />
+			<br/>
+			<label for="topic_description">Description</label>
+			<br/>
+			<textarea name="topic_description" rows="4" cols="50"></textarea>
+			<br/>
+			<label for="order">Order</label>
+			<br/>
+			<input type="text" name="order"/>
+			<br/>
+			<input type="hidden" name="course_code" value="<?php echo $course_code; ?>" />
+			<br/>
+			<input type="submit" value="Add Topic"/>
+		</form>
+	</div>
+</div>
