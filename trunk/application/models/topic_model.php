f1be96421fae70c3b981185f68d7e5bf99b60b98
shanchathuranga@gmail.com <shanchathuranga@gmail.com@e7e17b5c-1a57-c605-3fa1-0dffe02cf9ac>
2015-02-16 16:52:43 +0000

    topic added

---
+++ trunk/application/models/topic_model.php
@@ -0,0 +1,36 @@
+<?php
+
+class Topic_model extends CI_Model
+{
+	public function insert_topic()
+	{
+		$topic_title = $this->input->post('topic_title');
+		$topic_description = $this->input->post('topic_description');
+		$order = $this->input->post('order');
+		$course_code = $this->input->post('course_code');
+
+		$data = array(
+			'topic_title' => $topic_title,
+			'topic_description' => $topic_description,
+			'order' => $order,
+			'course_code' => $course_code,
+			'lecturer_reg_no' => $this->session->userdata('user_reg_no')
+		);
+
+		$this->db->insert('topic', $data);
+	}
+
+	public function get_all_topics_by_course_by_lecturer($course_code, $lecturer_reg_no)
+	{
+		$this->db->where('course_code', $course_code);
+		$this->db->where('lecturer_reg_no', $lecturer_reg_no);
+		$this->db->order_by('order', 'asc'); 
+		$result = $this->db->get('topic');
+		
+		if ( $result->num_rows() > 0 )
+		{
+			return $result->result();
+		}
+		return FALSE;
+	}
+}
