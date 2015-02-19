67a4c2e79515e7da5923e3ff3a77ce93ce064454
shanchathuranga <shanchathuranga@gmail.com>
2015-02-19 18:01:45 +0000

    student registration updated

--- application/views/student_register_form.php
+++ application/views/student_register_form.php
@@ -1,5 +1,10 @@
 <div class="grid_3">
-	&nbsp;
+	<div id="side-navigation">
+		<ul class="vert-menu">
+			<li class="vert-menu-header">Navigation</li>
+			<li><a href="<?php echo base_url(); ?>admin">My Home</a></li>
+		</ul>
+	</div>
 </div>
 <div class="grid_9">
 	<form action="<?php echo base_url(); ?>admin/student_regrister" method="post">
@@ -23,28 +28,41 @@
 		<br/>
 		<input type="text" name="contact_no">
 		<br/>
-                <label for="">Degree Program:</label>
-                <br/>
-                <select name="degree_code">
-                <?php
-                $result = $this->degree_model->get_all_degrees();
-                foreach ($result as $deg)
-                {
-                    echo '<option value="'.$deg->degree_code.'">'.$deg->degree_title.'</option>';
-                }
-                ?>
-                </select>
-                <br/>
-                <label for="">Year:</label>
-                <br/>
-                <select name="year">
-                    <option value="1">Year 1</option>
-                    <option value="2">Year 2</option>
-                    <option value="3">Year 3</option>
-                    <option value="4">Year 4</option>
-                </select>
-                <br/>
-                <br/>
+		<label for="">E-mail:</label>
+		<br/>
+		<input type="text" name="email">
+		<br/>
+		<label for="">Degree Program:</label>
+		<br/>
+		<select name="degree_code">
+			<?php
+			$result = $this->degree_model->get_all_degrees();
+			foreach ($result as $deg) {
+				echo '<option value="' . $deg->degree_code . '">' . $deg->degree_title . '</option>';
+			}
+			?>
+		</select>
+		<br/>
+		<label for="year">Year:</label>
+		<br/>
+		<select name="year">
+			<option value="1">Year 1</option>
+			<option value="2">Year 2</option>
+			<option value="3">Year 3</option>
+			<option value="4">Year 4</option>
+		</select>
+		<br/>
+		<label for="semester">Semester:</label>
+		<br/>
+		<select name="semester">
+			<option value="1">Semester 1</option>
+			<option value="2">Semester 2</option>
+		</select>
+		<br/>
+		<br/>
+		<input type="checkbox" name="add_user" value="1" /> Create new user for this student
+		<br/>
+		<br/>
 		<input type="submit" value="Register">
 		<br/>
 	</form>
