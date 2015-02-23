530ca20d2b7c7768a32f66a5f2396f8a4b6661f4
shanchathuranga@gmail.com <shanchathuranga@gmail.com@e7e17b5c-1a57-c605-3fa1-0dffe02cf9ac>
2015-02-23 15:50:08 +0000

    timetable upload function added

--- trunk/application/views/time_tables.php
+++ trunk/application/views/time_tables.php
@@ -38,5 +38,57 @@
 	<?php } ?>
 </div>
 <div class="grid_9">
-	Time Tables
+	<form action="<?php echo base_url().'lecturer/upload_timetables'; ?>" method="post" enctype="multipart/form-data">
+		<h4>Time Tables</h4>
+		<label>Title</label>
+		<br/>
+		<input type="text" name="title" />
+		<br/>
+		<label>Course</label>
+		<br/>
+		<select name="course_code">
+			<?php
+			$coursesList = $this->courselecturer_model->get_all_courses_by_lecturer($this->session->userdata('user_reg_no'));
+			if ($coursesList != FALSE)
+			{
+				foreach ($coursesList as $courseInfo)
+				{
+					$course = $this->course_model->get_course_by_code($courseInfo->course_code);
+					echo '<option value="'.$course->course_code.'">'.$course->course_title.'</option>';
+				}
+			}
+			?>
+		</select>
+		<br/>
+		<br/>
+		<input type="file" name="userfile" size="20" />
+		<br/>
+		<br/>
+		<input type="submit" value="Upload Timetable" name="submit" />
+	</form>
+	
+	<br/>
+	<hr/>
+	<h4>Current Timetables</h4>
+	
+	<?php
+	
+	$timetableList = $this->coursetimetable_model->get_timetables_by_lecturer($this->session->userdata('user_reg_no'));
+	if ($timetableList != FALSE)
+	{
+		echo '<table>';
+		foreach ($timetableList as $timetable)
+		{
+			$course = $this->course_model->get_course_by_code($timetable->course_code);
+			echo '<tr>';
+			echo '<td>'.$timetable->ct_title.'</td>';
+			echo '<td>'.$course->course_code.' / '.$course->course_title.'</td>';
+			echo '<td><a href="'.base_url().'lecturer/delete_timetable/'.$timetable->ct_id.'">Delete</a></td>';
+			echo '</tr>';
+		}
+		echo '</table>';
+	}
+	
+	?>
+	
 </div>
